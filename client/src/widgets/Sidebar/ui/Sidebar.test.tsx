80aa734d07bce22e2f6f7c41cf845bfeba5bf35d
ivan <lostnemo@rambler.ru>
2023-12-24 16:20:20 +0300

    Добавляет и нстраивает React Testing Library

---
+++ client/src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -0,0 +1,22 @@
+import {fireEvent, screen} from '@testing-library/react';
+import {Sidebar} from 'widgets/Sidebar';
+import {renderComponentWithProviders} from 'shared/lib/components/testing/renderComponentWithProviders';
+
+describe('Sidebar', () => {
+  test('with only first param', () => {
+    renderComponentWithProviders(<Sidebar />);
+    expect(screen.getByTestId('Sidebar')).toBeInTheDocument();
+  });
+
+  test('test toggle', () => {
+    renderComponentWithProviders(<Sidebar />);
+
+    const toggleBtn = screen.getByTestId('Sidebar-toggle');
+
+    fireEvent.click(toggleBtn);
+    expect(screen.getByTestId('Sidebar-toggle')).toHaveTextContent('Открыть');
+
+    fireEvent.click(toggleBtn);
+    expect(screen.getByTestId('Sidebar-toggle')).toHaveTextContent('Закрыть');
+  });
+});
--- client/src/widgets/Sidebar/ui/Sidebar.tsx
+++ client/src/widgets/Sidebar/ui/Sidebar.tsx
@@ -3,12 +3,14 @@ import cls from './Sidebar.module.scss';
 import classNames from 'classnames';
 import {Button} from 'shared/ui/Button/Button';
 import {Link} from 'react-router-dom';
+import {useTranslation} from 'react-i18next';
 
 const Sidebar = memo(() => {
   const [collapsed, setCollapsed] = useState(false);
+  const {t } = useTranslation();
 
   return (
-    <div className={classNames(cls.Sidebar, {[cls.SidebarCollapsed]: collapsed})}>
+    <div data-testid="Sidebar" className={classNames(cls.Sidebar, {[cls.SidebarCollapsed]: collapsed}, )}>
       <div className={cls.SidebarItem}>
         {collapsed ? <Link to={'/about'}>SD1</Link>: 'Sidebar item 1'}
       </div>
@@ -18,8 +20,8 @@ const Sidebar = memo(() => {
       <div className={cls.SidebarItem}>
         {collapsed ? 'SD3' : 'Sidebar item 3'}
       </div>
-      <Button onClick={() => setCollapsed(prev => !prev)}>
-        toggle
+      <Button data-testid="Sidebar-toggle" onClick={() => setCollapsed(prev => !prev)}>
+        {collapsed ? t('Открыть') : t('Закрыть')}
       </Button>
     </div>
   );
