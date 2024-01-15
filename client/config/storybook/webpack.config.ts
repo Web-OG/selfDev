cf1b1ad439326f4bdb1f86a15b5a4b01ab4b79b0
ivan <lostnemo@rambler.ru>
2024-01-15 10:10:26 +0300

    Добавляет строгий режим для typescript, правит появившиеся ошибки

--- client/config/storybook/webpack.config.ts
+++ client/config/storybook/webpack.config.ts
@@ -10,21 +10,21 @@ export default ({config}: { config: webpack.Configuration }) => {
     public: '',
     src: path.resolve(__dirname, '..', '..', 'src'),
   };
-  config.resolve.modules.push(paths.src);
-  config.resolve.extensions.push('.ts', '.tsx');
+  config!.resolve!.modules!.push(paths.src);
+  config!.resolve!.extensions!.push('.ts', '.tsx');
 
-  config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
-    if (/svg/.test(rule.test as string)) {
+  config!.module!.rules = config!.module!.rules!.map((rule: false | '' | 0 | RuleSetRule | '...' | null | undefined) => {
+    if (typeof rule === 'object' && /svg/.test(rule?.test as string)) {
       return {...rule, exclude: /\.svg$/i};
     }
 
     return rule;
   });
 
-  config.module.rules.push(...applySvgLoaders());
-  config.module.rules.push(applyCssLoaders(true));
+  config!.module!.rules!.push(...applySvgLoaders());
+  config!.module!.rules!.push(applyCssLoaders(true));
 
-  config.plugins.push(new DefinePlugin({
+  config!.plugins! .push(new DefinePlugin({
     __IS_DEV__: JSON.stringify(true),
     __API__: JSON.stringify(''),
     __PROJECT__: JSON.stringify('storybook'),
