660a765c7ae60e87a2caf502ad85db8c644143ed
ivan <lostnemo@rambler.ru>
2024-10-10 17:33:11 +0300

    добавляет глобальный импорт react

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,4 +1,6 @@
 import './App.scss';
+import React from 'react';
+import ReactDOM from 'react-dom'; // eslint-disable-line
 import {AppRouter} from './providers/AppRouter';
 import {useTheme} from 'shared/providers/ThemeProvider';
 import {userActions} from 'entities/User';
